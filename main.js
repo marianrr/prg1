b64937a3e6fc31ba046fc3470cf87c6170c86d36
emi web developer <marianseether@gmail.com>
2024-09-29 08:43:17 +0300

    function 3

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const Name = (name, age) => {
+// const Name = (name, age) => {
+//     return name + " " + age
+// }
+
+// console.log(Name("Mili", 23))
+
+const Name2 = (name, age) => {
+    this.name = "miau"
+    this.age = 99
     return name + " " + age
 }
 
-console.log(Name("Mili", 23))
+console.log(Name2("kikila", 55))
