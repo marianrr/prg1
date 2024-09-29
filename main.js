f990d82624a980fa0055ed983317476b84fba9dd
emi web developer <marianseether@gmail.com>
2024-09-29 10:56:11 +0300

    class 1

--- main.js
+++ main.js
@@ -4,10 +4,37 @@
 
 // console.log(Name("Mili", 23))
 
-const Name2 = (name, age) => {
-    this.name = "miau"
+// const Name2 = (name, age) => {
+//     this.name = "miau"
+//     this.age = 99
+//     return name + " " + age
+// }
+
+// console.log(Name2("kikila", 55))
+
+// const Name3 = (name, age) => {
+//     this.name = "miau"
+//     this.age = 99
+//     return this.name + " " + this.age
+// }
+
+// console.log(Name3("kikila", 55))
+
+class Name4 {
+    constructor(name) {
+        this.name = name
     this.age = 99
-    return name + " " + age
+    }
+    get name() {
+
+        return this._name
+    }
+
+set name(val) {
+this._name = val
 }
 
-console.log(Name2("kikila", 55))
+    
+}
+const soto = new Name4("kikila", 55)
+console.log(soto.name)
